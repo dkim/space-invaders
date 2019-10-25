1385322f0e92eb2be03171506fa06f32a9403d36
Deokhwan Kim <dkim@users.noreply.github.com>
2019-10-25 16:07:10 -0400

    feat: accept Space Invader ROM filenames as command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 #![warn(rust_2018_idioms)]
 
-fn main() {}
+use std::{path::PathBuf, process};
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about)]
+struct Opt {
+    /// A path to INVADERS.E
+    #[structopt(name = "INVADERS.E", parse(from_os_str))]
+    invaders_e: PathBuf,
+    /// A path to INVADERS.F
+    #[structopt(name = "INVADERS.F", parse(from_os_str))]
+    invaders_f: PathBuf,
+    /// A path to INVADERS.G
+    #[structopt(name = "INVADERS.G", parse(from_os_str))]
+    invaders_g: PathBuf,
+    /// A path to INVADERS.H
+    #[structopt(name = "INVADERS.H", parse(from_os_str))]
+    invaders_h: PathBuf,
+}
+
+fn main() {
+    if let Err(err) = run(Opt::from_args()) {
+        eprintln!("Error: {}", err);
+        process::exit(1);
+    }
+}
+
+fn run(_opt: Opt) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
